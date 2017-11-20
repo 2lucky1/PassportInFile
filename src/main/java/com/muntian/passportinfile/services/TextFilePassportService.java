3c900effe5a2bd10434b8c0a0862d02ce530a736
Nikolai Muntian <lanikolya1989@gmail.com>
2017-11-20 18:54:29 +0200

    Passed accessors into constructor of TextFilePassportService

--- src/main/java/com/muntian/passportinfile/services/TextFilePassportService.java
+++ src/main/java/com/muntian/passportinfile/services/TextFilePassportService.java
@@ -4,30 +4,28 @@ import com.muntian.passportinfile.entities.Passport;
 import com.muntian.passportinfile.entities.Visa;
 import com.muntian.passportinfile.services.accessors.PassportAccessor;
 import com.muntian.passportinfile.services.accessors.VisaAccessor;
-import com.muntian.passportinfile.services.accessors.impl.TextFilePassportAccessor;
-import com.muntian.passportinfile.services.accessors.impl.TextFileVisaAccessor;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 //This class is my code
 public class TextFilePassportService implements PassportService {
     private PassportAccessor passportAccessor;
     private VisaAccessor visaAccessor;
-    private Collection<Passport> passports;
-    private Collection<Visa> visas;
-    private Map<String, Visa> visaMap;
 
-    public TextFilePassportService(String passportFileName, String visaFileName) {
-        this.passportAccessor = new TextFilePassportAccessor(passportFileName);
-
-        this.visaAccessor = new TextFileVisaAccessor(visaFileName);
-        this.passports = passportAccessor.readAll();
-        this.visas = visaAccessor.readAll();
-        this.visaMap = new HashMap<>();
+    public TextFilePassportService(PassportAccessor passportAccessor, VisaAccessor visaAccessor,
+                                   String passportFileName, String visaFileName) {
+        this.passportAccessor = passportAccessor;
+        this.visaAccessor = visaAccessor;
     }
 
     @Override
     public Collection<Passport> readAll() {
+        Map<String, Visa> visaMap = new HashMap<>();
+        Collection<Visa> visas = visaAccessor.readAll();
+        Collection<Passport> passports = passportAccessor.readAll();
+
         for (Visa visa : visas) {
             visaMap.put(visa.getPassportNumber(), visa);
         }
